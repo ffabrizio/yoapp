48979335ec3c2c56eb88c1eaecd0f77accf9c623
Fabio <ffabrizio@gmail.com>
2014-01-13 22:12:34 +0000

    yo bbb app started

---
+++ app/modules/home.js
@@ -0,0 +1,22 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  // External dependencies.
+  var _ = require("underscore");
+  var $ = require("jquery");
+  var Backbone = require("backbone");
+  var template = require("text!templates/home.html");
+
+  module.exports = Backbone.View.extend({
+    initialize: function(){
+      this.model = {
+        pageTitle: "Hallo hallo...",
+        copyrightMessage: "Fabs " + new Date().getFullYear()
+      };
+      this.render();
+    },
+    render: function(content){
+      this.$el.html(_.template(template, this.model));
+    }
+  });
+});
---
+++ app/router.js
@@ -0,0 +1,20 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  // External dependencies.
+  var Backbone = require("backbone");
+  var Home = require("modules/home");
+
+  // Defining the application router.
+  module.exports = Backbone.Router.extend({
+    routes: {
+      "": "index"
+    },
+
+    index: function() {
+      console.log("Welcome to your / route.");
+
+      var home = new Home({ el : '#main'});
+    }
+  });
+});
